7ee7f9b81aa15244ac7cd41925874d552df9d905
wybosys <wybosys@gmail.com>
2022-03-30 16:57:05 +0800

    prj

--- src/android-images.cpp
+++ src/android-images.cpp
@@ -1,1 +1,6 @@
 #include "android-images.hpp"
+
+void png_write_aapt_file(png_structp png_ptr, png_bytep data, png_size_t length)
+{
+    
+}
--- src/android-images.hpp
+++ src/android-images.hpp
@@ -1,4 +1,9 @@
 #ifndef __ANDROID_IMAGES_H_INCLUDED
 #define __ANDROID_IMAGES_H_INCLUDED
 
+#define PNG_INTERNAL
+#include <png.h>
+
+extern void png_write_aapt_file(png_structp png_ptr, png_bytep data, png_size_t length);
+
 #endif
